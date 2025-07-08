fa0cd24dc0259cc7a312fcfc0edf2c3ca73f04e1
azmanabdlh <azmanabdlh12@gmail.com>
2025-07-08 21:42:16 +0900

    fix sluggable

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -2,18 +2,18 @@ module Sluggable
   extend ActiveSupport::Concern
 
   class_methods do
-    def sluggable(column = :title, length:)
+    def sluggable(column = :title, **options)
+      len = options[:length] or 20
       before_save do
-        generate_slug(column, length)
+        generate_slug(column, len)
       end
     end
   end
 
   private
-  def generate_slug(column, length)
+  def generate_slug(column, len)
     # this func to generate slug by title
     # ex: blabla-blabla-43289472893
-    len = length or 20
     t = send(column).to_s
     t = t.split().first if t.split().length == 1
     t = t.first len if t.length > len
