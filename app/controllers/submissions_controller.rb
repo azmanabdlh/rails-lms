a2ce8fa2ca2c0760add0414fe14bb7cb94015cd0
Azman <macbookpro@MacBook-Pro-13.local>
2025-07-21 18:32:26 +0900

    add create submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -4,7 +4,21 @@ class SubmissionsController < ApplicationController
     @exams = @submission.exams
   end
 
+  def create
+    submission = Current.user.submission.new(
+      course_id: params[:course_id],
+      started_at: Time.current,
+    )
+    if submission.save
+      render :show, status: :created
+    else
+      render json: { message: "invalid create submission", errors: submission.errors }, status: :unprocessable_entity
+    end
+  end
+
   private
+
+
   def authorize_submission
     id = params[:id]
     @submission = Submission.find(id)
